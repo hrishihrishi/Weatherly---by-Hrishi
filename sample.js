51de4124bb05b6e16ff5074392946c5217f918d9
Hrishikesh <hrishi.halesh.18@gmail.com>
2025-12-10 19:38:52 +0530

    restructured folders

--- sample.js
+++
@@ -1,9 +0,0 @@
-// Event listener for the dropdown
-recentCities.addEventListener("change", function () {
-    const selectedCity = this.value;
-    if (selectedCity) {
-        cityInput.value = selectedCity;
-        disableButtons();
-        fetchWeatherData(selectedCity);
-    }
-});
